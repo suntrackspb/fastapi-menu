fd3113faae5a51c7d95c8d6bc04809489f0aa347
suntrackspb <spbwar@gmail.com>
2023-08-05 18:18:49 +0300

    settings pre-commit

--- app/config.py
+++ app/config.py
@@ -1,18 +1,19 @@
 import os
+
 from dotenv import load_dotenv
 
 BASEDIR: str = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-load_dotenv(os.path.join(BASEDIR, "../.env"))
+load_dotenv(os.path.join(BASEDIR, '../.env'))
 
-if os.getenv("MODE") == "TEST":
-    DB_NAME: str = os.getenv("DB_NAME_TEST", default="postgres")
-    DB_USER: str = os.getenv("DB_USER_TEST", default="postgres")
-    DB_PASS: str = os.getenv("DB_PASS_TEST", default="postgres")
-    DB_HOST: str = os.getenv("DB_HOST_TEST", default="localhost")
-    DB_PORT: int = int(os.getenv("DB_PORT_TEST", default="6543"))
+if os.getenv('MODE') == 'TEST':
+    env_prefix = '_TEST'
 else:
-    DB_NAME: str = os.getenv("DB_NAME", default="postgres")
-    DB_USER: str = os.getenv("DB_USER", default="postgres")
-    DB_PASS: str = os.getenv("DB_PASS", default="postgres")
-    DB_HOST: str = os.getenv("DB_HOST", default="localhost")
-    DB_PORT: int = int(os.getenv("DB_PORT", default="5432"))
+    env_prefix = ''
+
+DB_NAME: str = os.getenv(f'DB_NAME{env_prefix}', default='postgres')
+DB_USER: str = os.getenv(f'DB_USER{env_prefix}', default='postgres')
+DB_PASS: str = os.getenv(f'DB_PASS{env_prefix}', default='postgres')
+DB_HOST: str = os.getenv(f'DB_HOST{env_prefix}', default='localhost')
+DB_PORT: int = int(os.getenv(f'DB_PORT{env_prefix}', default='5432'))
+
+REDIS_URL = f"redis://{os.getenv('REDIS_HOST')}{os.getenv('REDIS_PORT')}"
